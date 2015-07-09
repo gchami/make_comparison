3d5019ac4536286ab8ade68d8b1da982c1feb589
slaffer-au <slaffer.au@gmail.com>
2015-07-09 15:21:28 -0400

    test1

--- make_comparison.py
+++ make_comparison.py
@@ -1,3 +1,14 @@
 #!/bin/python
 
 import os
+import sys
+
+base1 = open("/mnt/behemoth1/customers_delivered_releases/2.5/2.5.3/build_20150601181714_22e6f85/build-config/make/local-pkgs.make")
+base2 = open("/mnt/behemoth1/customers_delivered_releases/2.5/2.5.3/build_20150601181714_22e6f85/build-config/make/local-pkgs2.make")
+
+print base1
+print base2
+
+
+base1.close()
+base2.close()
